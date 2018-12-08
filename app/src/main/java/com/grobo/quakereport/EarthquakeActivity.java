63e03407ee24c98120532d5626be96c11f24fc2d
amangrobo <amangrobo@gmail.com>
2018-12-08 09:12:01 +0530

    changed Async to loader, progressbar added, no internet solution, no data solution

--- app/src/main/java/com/grobo/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/grobo/quakereport/EarthquakeActivity.java
@@ -1,23 +1,31 @@
 package com.grobo.quakereport;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
-import static com.grobo.quakereport.QueryUtils.doEverything;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<List<Quake>> {
 
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=100";
 
     private ListItemAdapter mAdapter;
+    private TextView emptyView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,8 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         ListView quakeListView = (ListView) findViewById(R.id.quake_list_view);
+        emptyView = (TextView) findViewById(R.id.empty_view);
+        quakeListView.setEmptyView(emptyView);
 
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new ListItemAdapter(this, new ArrayList<Quake>());
@@ -40,31 +50,41 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
 
-        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null;
+
+        if (isConnected) {
+            LoaderManager loaderManager = android.support.v4.app.LoaderManager.getInstance(this);
+            loaderManager.initLoader(1, null, this);
+        } else {
+            progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+            progressBar.setVisibility(View.GONE);
+            emptyView.setText(R.string.no_internet);
+        }
     }
 
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Quake>> {
+    @Override
+    public Loader<List<Quake>> onCreateLoader(int i, Bundle bundle) {
+        return new EarthquakeLoader(this, USGS_REQUEST_URL);
+    }
 
-        @Override
-        protected List<Quake> doInBackground(String... urls) {
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-            List<Quake> listEarthquake = doEverything(urls[0]);
-            return  listEarthquake;
-        }
+    @Override
+    public void onLoaderReset(Loader<List<Quake>> loader) {
+        mAdapter.clear();
+    }
 
-        @Override
-        protected void onPostExecute(List<Quake> data) {
+    @Override
+    public void onLoadFinished(Loader<List<Quake>> loader, List<Quake> quakes) {
+        mAdapter.clear();
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.GONE);
+        emptyView.setText(R.string.no_data);
 
-            mAdapter.clear();
-
-            if (data != null && !data.isEmpty()){
-                mAdapter.addAll(data);
-            }
-
+        if (quakes != null && !quakes.isEmpty()){
+            mAdapter.addAll(quakes);
         }
 
     }
+
 }
